43883fbefd28a17306bfb007b626b19fab5d033c
Anastasia Pakkar <shivasmile11@gmail.com>
2019-10-13 22:19:24 +0300

    first commit

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import List from '../List'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header >
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <List></List>
     </div>
   );
 }
---
+++ src/Components/List/index.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+
+import './index.css';
+class List extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        currencies: [
+            { ticker: 'btc', name: 'bitok' },
+            { ticker: 'storm', name: 'storm' },
+            { ticker: 'atom', name: 'atom' },
+        ],
+        currentIndex: 0,
+
+    }
+
+    handleLog = () => {
+        console.log('logged');
+    }
+
+    handleListKeyPress = (event) => {
+        // 13 - enter, 38 - up, 40 - down
+        const { currencies } = this.state;
+        const maxItems = currencies.length - 1;
+
+
+        console.log('event keycode', event.keyCode)
+
+        switch (event.keyCode) {
+            case 13:
+
+                break;
+            case 38:
+
+                if (this.state.currentIndex > 0) {
+                    console.log()
+                    this.setState(state => ({ currentIndex: state.currentIndex - 1 }));
+                }
+                console.log(this.state.currentIndex)
+                console.log('key up')
+                break;
+
+            case 40:
+                if (this.state.currentIndex < maxItems) {
+                    console.log('maxItems', maxItems)
+                    this.setState(state => ({ currentIndex: state.currentIndex + 1 }));
+                }
+
+                console.log(this.state.currentIndex)
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+
+    render() {
+
+        const listCurrency = this.state.currencies.map((el, i) => {
+            let activeEl = this.state.currentIndex === i ? 'active' : ''
+            return (
+                <li key={el.name} className={`list__item ${activeEl}`}>
+                    {el.ticker}
+                </li>
+            );
+        }
+        )
+
+        return (
+            <div
+                className="List"
+                onKeyDown={this.handleListKeyPress}
+                tabIndex="0"
+            >
+                <ul>
+                    {listCurrency}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default List;
